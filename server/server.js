4bd14736032ce3edea3d0587a907f9b952f26b31
rahulpurshottam <r77933079@gmail.com>
2025-06-18 22:47:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js';
 import userRouter from './routes/userRoutes.js';
+import imageRouter from './routes/imageRoutes.js';
 
 
 const PORT=process.env.PORT || 4000;
@@ -14,6 +15,7 @@ app.use(cors())
 
 app.get('/',(req,res)=>res.send("API Working!"))
 app.use('/api/user',userRouter)
+app.use('/api/image',imageRouter)
 
 
 app.listen(PORT,()=>console.log("Server running on port "+PORT))
