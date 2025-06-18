2fec0ff73d6546e37bd32908471a7ea1217ecb41
rahulpurshottam <r77933079@gmail.com>
2025-06-18 10:33:54 +0530

    Delete server/routes/userRoutes.js

--- server/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import {clerkWebHooks} from '../controllers/userController.js'
-
-const userRouter=express.Router()
-userRouter.post('/webhooks',clerkWebHooks)
-
-export default userRouter;
