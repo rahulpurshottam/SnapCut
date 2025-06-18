d596d5d9a15d37623423161202e9df1f1957f8e1
rahulpurshottam <r77933079@gmail.com>
2025-06-18 23:32:16 +0530

    Update postcss.config.mjs

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,7 +1,8 @@
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
 const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss(), autoprefixer()],
 };
+
 export default config;
