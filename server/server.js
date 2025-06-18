3361ba2d39013b24262160377efadccb09564ba0
rahulpurshottam <r77933079@gmail.com>
2025-06-18 11:14:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js';
+import userRouter from './routes/userRoutes.js';
 
 
 const PORT=process.env.PORT || 4000;
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(req,res)=>res.send("API Working!"))
+app.use('/api/user',userRouter)
 
 
 app.listen(PORT,()=>console.log("Server running on port "+PORT))
